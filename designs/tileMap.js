7e6f6ad8b0f88dfd49c6bcb2f2d0e673f08fd3d7
Blol22ck <117266908+Blol22ck@users.noreply.github.com>
2023-05-03 15:01:49 +0200

    Working with tilemap

--- designs/tileMap.js
+++ designs/tileMap.js
@@ -1,58 +1,441 @@
-// let tileMap = {
-//   column: 10,
-//   row: 10,
-//   tilesize: 50,
-//   layers: [
-//     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-//     [
-//       20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 38,
-//       39, 40,
-//     ],
-//   ],
-// };
+const size = 90;
+const length = 10;
+let wallX = 100;
+let wallY = 100;
+noStroke();
 
-var map = {
-  cols: 8,
-  rows: 8,
-  tsize: 64,
-  tiles: [
-    1, 3, 3, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1,
-    1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-  ],
-  getTile: function (col, row) {
-    return this.tiles[row * map.cols + col];
-  },
-};
+function straightWall(wallX, wallY) {
+  //OUTLINES
+  fill(40, 40, 60);
 
-Game.load = function () {
-  return [Loader.loadImage("tiles", "...designs/tiles.png")];
-};
+  rect(wallX + 80, wallY, 5);
+  rect(wallX + 80, wallY + 5, 5);
+  rect(wallX + 80, wallY + 10, 5);
+  rect(wallX + 80, wallY + 30, 5);
+  rect(wallX + 80, wallY + 35, 5);
+  rect(wallX + 80, wallY + 40, 5);
 
-Game.init = function () {
-  this.tileAtlas = Loader.getImage("tiles");
-};
+  rect(wallX + 35, wallY, 5);
+  rect(wallX + 35, wallY + 5, 5);
+  rect(wallX + 35, wallY + 10, 5);
 
-Game.update = function (delta) {};
+  rect(wallX - 10, wallY, 5);
+  rect(wallX - 10, wallY + 5, 5);
+  rect(wallX - 10, wallY + 10, 5);
+  rect(wallX - 10, wallY + 30, 5);
+  rect(wallX - 10, wallY + 35, 5);
+  rect(wallX - 10, wallY + 40, 5);
 
-Game.render = function () {
-  for (var c = 0; c < map.cols; c++) {
-    for (var r = 0; r < map.rows; r++) {
-      var tile = map.getTile(c, r);
-      if (tile !== 0) {
-        // 0 => empty tile
-        this.ctx.drawImage(
-          this.tileAtlas, // image
-          (tile - 1) * map.tsize, // source x
-          0, // source y
-          map.tsize, // source width
-          map.tsize, // source height
-          c * map.tsize, // target x
-          r * map.tsize, // target y
-          map.tsize, // target width
-          map.tsize // target height
-        );
-      }
-    }
+  push();
+  fill(20, 20, 40);
+  rect(wallX + 80, wallY + 15, 5);
+  rect(wallX + 80, wallY + 20, 5);
+  rect(wallX + 80, wallY + 25, 5);
+
+  rect(wallX + 35, wallY + 15, 5);
+  rect(wallX + 35, wallY + 20, 5);
+  rect(wallX + 35, wallY + 25, 5);
+
+  rect(wallX - 10, wallY + 15, 5);
+  rect(wallX - 10, wallY + 20, 5);
+  rect(wallX - 10, wallY + 25, 5);
+  pop();
+
+  push();
+
+  fill(20, 20, 40);
+  rect(wallX + 75, wallY + 25, 5);
+  rect(wallX + 70, wallY + 25, 5);
+  rect(wallX + 65, wallY + 25, 5);
+
+  pop();
+
+  push();
+
+  fill(40, 40, 50);
+  rect(wallX + 60, wallY + 25, 5);
+  rect(wallX + 55, wallY + 25, 5);
+  rect(wallX + 50, wallY + 25, 5);
+  rect(wallX + 45, wallY + 25, 5);
+  rect(wallX + 40, wallY + 25, 5);
+
+  pop();
+
+  push();
+
+  fill(20, 20, 40);
+  rect(wallX + 30, wallY + 25, 5);
+  rect(wallX + 25, wallY + 25, 5);
+  rect(wallX + 20, wallY + 25, 5);
+
+  pop();
+
+  push();
+
+  fill(40, 40, 50);
+  rect(wallX + 15, wallY + 25, 5);
+  rect(wallX + 10, wallY + 25, 5);
+  rect(wallX + 5, wallY + 25, 5);
+  rect(wallX, wallY + 25, 5);
+  rect(wallX - 5, wallY + 25, 5);
+
+  pop();
+
+  push();
+
+  fill(20, 20, 40);
+  rect(wallX + 30, wallY + 45, 5);
+  rect(wallX + 25, wallY + 45, 5);
+  rect(wallX + 20, wallY + 45, 5);
+
+  pop();
+
+  push();
+
+  fill(40, 40, 50);
+  rect(wallX + 15, wallY + 45, 5);
+  rect(wallX + 10, wallY + 45, 5);
+  rect(wallX + 5, wallY + 45, 5);
+  rect(wallX, wallY + 45, 5);
+  rect(wallX - 5, wallY + 45, 5);
+  rect(wallX - 10, wallY + 45, 5);
+
+  pop();
+
+  push();
+
+  fill(20, 20, 40);
+  rect(wallX + 45, wallY + 45, 5);
+  rect(wallX + 50, wallY + 45, 5);
+  rect(wallX + 55, wallY + 45, 5);
+
+  pop();
+
+  push();
+
+  fill(40, 40, 50);
+  rect(wallX + 60, wallY + 45, 5);
+  rect(wallX + 65, wallY + 45, 5);
+  rect(wallX + 70, wallY + 45, 5);
+  rect(wallX + 75, wallY + 45, 5);
+  rect(wallX + 80, wallY + 45, 5);
+  rect(wallX + 40, wallY + 45, 5);
+  rect(wallX + 35, wallY + 45, 5);
+
+  pop();
+
+  push();
+
+  fill(40, 40, 50);
+  rect(wallX + 15, wallY + 40, 5);
+  rect(wallX + 15, wallY + 35, 5);
+  rect(wallX + 15, wallY + 30, 5);
+
+  pop();
+
+  push();
+
+  fill(20, 20, 40);
+  rect(wallX + 55, wallY + 40, 5);
+  rect(wallX + 55, wallY + 35, 5);
+  rect(wallX + 55, wallY + 30, 5);
+
+  pop();
+
+  //FILLING
+
+  push();
+
+  fill(150, 150, 180);
+  rect(wallX + 40, wallY, 5);
+  rect(wallX + 40, wallY + 5, 5);
+  rect(wallX + 45, wallY + 5, 5);
+  rect(wallX + 50, wallY + 5, 5);
+  rect(wallX + 55, wallY + 5, 5);
+  rect(wallX + 60, wallY + 5, 5);
+  rect(wallX + 65, wallY + 5, 5);
+  rect(wallX + 70, wallY + 5, 5);
+  rect(wallX + 75, wallY + 5, 5);
+
+  rect(wallX + 75, wallY, 5);
+  rect(wallX + 70, wallY, 5);
+  rect(wallX + 65, wallY, 5);
+  rect(wallX + 60, wallY, 5);
+  rect(wallX + 55, wallY, 5);
+  rect(wallX + 50, wallY, 5);
+  rect(wallX + 45, wallY, 5);
+
+  //Left Side
+  rect(wallX - 5, wallY, 5);
+  rect(wallX, wallY, 5);
+  rect(wallX + 5, wallY, 5);
+  rect(wallX + 10, wallY, 5);
+  rect(wallX + 15, wallY, 5);
+  rect(wallX + 20, wallY, 5);
+  rect(wallX + 25, wallY, 5);
+  rect(wallX + 30, wallY, 5);
+
+  rect(wallX - 5, wallY + 5, 5);
+  rect(wallX, wallY + 5, 5);
+  rect(wallX + 5, wallY + 5, 5);
+  rect(wallX + 10, wallY + 5, 5);
+  rect(wallX + 15, wallY + 5, 5);
+  rect(wallX + 20, wallY + 5, 5);
+  rect(wallX + 25, wallY + 5, 5);
+  rect(wallX + 30, wallY + 5, 5);
+
+  //Dark Under
+
+  push();
+  fill(100, 100, 140);
+
+  //Left
+  push();
+  fill(200, 200, 210);
+  rect(wallX - 5, wallY + 10, 5);
+  rect(wallX, wallY + 10, 5);
+  rect(wallX + 5, wallY + 10, 5);
+  rect(wallX + 10, wallY + 10, 5);
+  rect(wallX + 15, wallY + 10, 5);
+  rect(wallX + 20, wallY + 10, 5);
+  rect(wallX + 25, wallY + 10, 5);
+  rect(wallX + 30, wallY + 10, 5);
+  pop();
+
+  rect(wallX - 5, wallY + 15, 5);
+  rect(wallX, wallY + 15, 5);
+  rect(wallX + 5, wallY + 15, 5);
+  rect(wallX + 10, wallY + 15, 5);
+  rect(wallX + 15, wallY + 15, 5);
+  rect(wallX + 20, wallY + 15, 5);
+  rect(wallX + 25, wallY + 15, 5);
+  rect(wallX + 30, wallY + 15, 5);
+
+  rect(wallX - 5, wallY + 20, 5);
+  rect(wallX, wallY + 20, 5);
+  rect(wallX + 5, wallY + 20, 5);
+  rect(wallX + 10, wallY + 20, 5);
+  rect(wallX + 15, wallY + 20, 5);
+  rect(wallX + 20, wallY + 20, 5);
+  rect(wallX + 25, wallY + 20, 5);
+  rect(wallX + 30, wallY + 20, 5);
+
+  //Right
+  push();
+  fill(200, 200, 210);
+  rect(wallX + 40, wallY + 10, 5);
+  rect(wallX + 45, wallY + 10, 5);
+  rect(wallX + 50, wallY + 10, 5);
+  rect(wallX + 55, wallY + 10, 5);
+  rect(wallX + 60, wallY + 10, 5);
+  rect(wallX + 65, wallY + 10, 5);
+  rect(wallX + 70, wallY + 10, 5);
+  rect(wallX + 75, wallY + 10, 5);
+  pop();
+
+  rect(wallX + 40, wallY + 15, 5);
+  rect(wallX + 45, wallY + 15, 5);
+  rect(wallX + 50, wallY + 15, 5);
+  rect(wallX + 55, wallY + 15, 5);
+  rect(wallX + 60, wallY + 15, 5);
+  rect(wallX + 65, wallY + 15, 5);
+  rect(wallX + 70, wallY + 15, 5);
+  rect(wallX + 75, wallY + 15, 5);
+
+  rect(wallX + 40, wallY + 20, 5);
+  rect(wallX + 45, wallY + 20, 5);
+  rect(wallX + 50, wallY + 20, 5);
+  rect(wallX + 55, wallY + 20, 5);
+  rect(wallX + 60, wallY + 20, 5);
+  rect(wallX + 65, wallY + 20, 5);
+  rect(wallX + 70, wallY + 20, 5);
+  rect(wallX + 75, wallY + 20, 5);
+  pop();
+
+  //Bottom Tiles
+
+  push();
+
+  fill(100, 100, 140);
+  rect(wallX - 5, wallY + 30, 5);
+  rect(wallX, wallY + 30, 5);
+  rect(wallX + 5, wallY + 30, 5);
+  rect(wallX + 10, wallY + 30, 5);
+  rect(wallX + 20, wallY + 30, 5);
+  rect(wallX + 25, wallY + 30, 5);
+  rect(wallX + 30, wallY + 30, 5);
+  rect(wallX + 35, wallY + 30, 5);
+  rect(wallX + 40, wallY + 30, 5);
+  rect(wallX + 45, wallY + 30, 5);
+  rect(wallX + 50, wallY + 30, 5);
+
+  rect(wallX + 60, wallY + 30, 5);
+  rect(wallX + 65, wallY + 30, 5);
+  rect(wallX + 70, wallY + 30, 5);
+  rect(wallX + 75, wallY + 30, 5);
+
+  pop();
+
+  //Darker Under Bottom Tiles
+
+  push();
+
+  //Line 1
+  fill(70, 70, 100);
+  rect(wallX - 5, wallY + 35, 5);
+  rect(wallX, wallY + 35, 5);
+  rect(wallX + 5, wallY + 35, 5);
+  rect(wallX + 10, wallY + 35, 5);
+  rect(wallX + 20, wallY + 35, 5);
+  rect(wallX + 25, wallY + 35, 5);
+  rect(wallX + 30, wallY + 35, 5);
+  rect(wallX + 35, wallY + 35, 5);
+  rect(wallX + 40, wallY + 35, 5);
+  rect(wallX + 45, wallY + 35, 5);
+  rect(wallX + 50, wallY + 35, 5);
+
+  rect(wallX + 60, wallY + 35, 5);
+  rect(wallX + 65, wallY + 35, 5);
+  rect(wallX + 70, wallY + 35, 5);
+  rect(wallX + 75, wallY + 35, 5);
+
+  //Line 2
+  rect(wallX - 5, wallY + 40, 5);
+  rect(wallX, wallY + 40, 5);
+  rect(wallX + 5, wallY + 40, 5);
+  rect(wallX + 10, wallY + 40, 5);
+  rect(wallX + 20, wallY + 40, 5);
+  rect(wallX + 25, wallY + 40, 5);
+  rect(wallX + 30, wallY + 40, 5);
+  rect(wallX + 35, wallY + 40, 5);
+  rect(wallX + 40, wallY + 40, 5);
+  rect(wallX + 45, wallY + 40, 5);
+  rect(wallX + 50, wallY + 40, 5);
+
+  rect(wallX + 60, wallY + 40, 5);
+  rect(wallX + 65, wallY + 40, 5);
+  rect(wallX + 70, wallY + 40, 5);
+  rect(wallX + 75, wallY + 40, 5);
+
+  pop();
+
+  //Extra Detail Pixels
+  push();
+  fill(170, 170, 200);
+  rect(wallX + 55, wallY, 5);
+  rect(wallX + 65, wallY + 5, 5);
+  rect(wallX + 25, wallY, 5);
+  rect(wallX + 5, wallY + 5, 5);
+
+  fill(120, 120, 150);
+  rect(wallX + 30, wallY + 5, 5);
+  rect(wallX - 5, wallY, 5);
+  rect(wallX + 75, wallY + 5, 5);
+
+  fill(90, 90, 110);
+  rect(wallX + 45, wallY + 20, 5);
+  rect(wallX + 20, wallY + 20, 5);
+  rect(wallX + 15, wallY + 20, 5);
+
+  fill(120, 120, 180);
+  rect(wallX + 5, wallY + 30, 5);
+  rect(wallX - 5, wallY + 30, 5);
+  rect(wallX + 30, wallY + 30, 5);
+  rect(wallX + 35, wallY + 30, 5);
+  rect(wallX + 65, wallY + 30, 5);
+
+  fill(60, 60, 90);
+  rect(wallX + 75, wallY + 35, 5);
+  rect(wallX + 70, wallY + 35, 5);
+  rect(wallX + 45, wallY + 35, 5);
+  rect(wallX + 30, wallY + 35, 5);
+  rect(wallX + 25, wallY + 35, 5);
+  rect(wallX, wallY + 35, 5);
+  rect(wallX + 50, wallY + 40, 5);
+  rect(wallX + 35, wallY + 40, 5);
+  rect(wallX + 60, wallY + 40, 5);
+  rect(wallX + 10, wallY + 40, 5);
+
+  fill(40, 40, 70);
+  rect(wallX - 5, wallY + 40, 5);
+  rect(wallX + 25, wallY + 40, 5);
+  rect(wallX + 60, wallY + 35, 5);
+
+  fill(70, 70, 90);
+  rect(wallX + 75, wallY + 20, 5);
+  rect(wallX + 70, wallY + 20, 5);
+  rect(wallX + 75, wallY + 15, 5);
+  rect(wallX - 5, wallY + 15, 5);
+  rect(wallX - 5, wallY + 20, 5);
+  rect(wallX + 30, wallY + 20, 5);
+  rect(wallX + 15, wallY + 15, 5);
+  rect(wallX + 10, wallY + 15, 5);
+
+  fill(240, 240, 255);
+  rect(wallX + 5, wallY + 10, 5);
+  rect(wallX + 25, wallY + 10, 5);
+  rect(wallX + 20, wallY + 10, 5);
+  rect(wallX + 45, wallY + 10, 5);
+  rect(wallX + 50, wallY + 10, 5);
+  rect(wallX + 70, wallY + 10, 5);
+
+  fill(20, 100, 20);
+  rect(wallX + 65, wallY + 15, 5);
+  rect(wallX + 60, wallY + 15, 5);
+  rect(wallX + 55, wallY + 15, 5);
+  rect(wallX + 60, wallY + 20, 5);
+
+  rect(wallX + 20, wallY + 15, 5);
+  rect(wallX + 5, wallY + 15, 5);
+  rect(wallX, wallY + 15, 5);
+
+  rect(wallX + 20, wallY + 30, 5);
+  rect(wallX + 50, wallY + 30, 5);
+  pop();
+
+  pop();
+}
+
+let tileMap = [0, 1, 2, 3, 4, 5];
+
+// for (let x = 0; x < length; x++) {
+//   for (let y = 0; y < length; y++) {
+//     if (y % 2 === 0) {
+//       if (x % 2 === 0) {
+//         fill(0, 0, 0);
+//       } else {
+//         fill(255, 255, 255);
+//       }
+//     } else {
+//       if (x % 2 === 0) {
+//         fill(255, 255, 255);
+//       } else {
+//         fill(0, 0, 0);
+//       }
+//     }
+
+//     straightWall(x * size, y * size, size, size);
+//   }
+// }
+
+let animals = [0, 1, 2, 0, 1, 2];
+
+function draw() {
+  background(128, 128, 128);
+  textSize(20);
+  let y = 20;
+  let x = 20;
+  for (let animal of animals) {
+    text(animal, 20, y);
+    y = y + 70;
   }
-};
+  if (animals === 1) {
+    straightWall(10, 10);
+  }
+}
+
+// lägga ett object i en array, sen göra en for
+// loop som loopar igenom och kollar alla värden för att
+// veta om det är en vägg. Sedan If statement som gör
+// att gubben krockar i väggen.
+//let wall1 = [{x:10, y:10, width:20, height:20}];
